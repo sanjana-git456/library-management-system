cb43a242a102dfea72a2027c0708b9ab58d8ec4c
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-07-12 03:27:51 +0530

    librarian

---
+++ Librarian.java
@@ -0,0 +1,12 @@
+public class Librarian extends User{
+    public Librarian(String name, int userId, String email) {
+        super(name, userId, email);
+    }
+
+    @Override
+    public void showMenu() {
+        System.out.println("Librarian Menu");
+        System.out.println("Name: " + getName() + " | ID: " + getId());
+        System.out.println("1. Add Books  2. Remove Book  3. View Circulation Records");
+    }
+}
