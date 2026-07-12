a518e59213c88d298188088ea2ab383e58673066
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-07-12 17:24:30 +0530

    add, find, remove book

---
+++ Library.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Library {
+    private List<Book> books;
+
+    public Library() {
+        this.books = new ArrayList<>();
+    }
+    public void addBook(Book book) {
+        books.add(book);
+    }
+    public Book findbyIsbn(int isbn) {
+        for (Book book : books) {
+            if (book.getIsbn() == isbn) {
+                return book;
+            }
+        }
+        return null;
+    }
+    public void removeBook(int isbn) {
+        Book book = findbyIsbn(isbn);
+        if (book == null) {
+            System.out.println("Book with isbn " + isbn + "is not there");
+        }
+        else {
+            books.remove(book);
+        }
+    }
+}
