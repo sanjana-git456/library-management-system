df32d9be41ddd73b7cd4633113ddb0b8740bfe7f
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-07-13 22:39:18 +0530

    full customer in main

--- Customer.java
+++ Customer.java
@@ -1,17 +1,20 @@
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class Customer extends User{
+public class Customer extends User {
+
     private List<Book> borrowedBooks;
 
     public Customer(String name, int userId, String email) {
         super(name, userId, email);
         this.borrowedBooks = new ArrayList<>();
     }
+
     @Override
     public void showMenu() {
         System.out.println("Customer Menu");
         System.out.println("Name: " + getName() + " | ID: " + getId());
-        System.out.println("1. Browse Books  2. Checkout Book  3. Return Book");
+        System.out.println("1. Browse Books  2. Checkout Book  3. Return Book  4. Exit");
     }
 }
--- Main.java
+++ Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.println("Welcome to the Library System");
         System.out.println("1. Login as Customer");
         System.out.println("2. Login as Librarian");
+        System.out.println("3. Exit");
         System.out.println("Enter your choice: ");
         int choice = scanner.nextInt();
         if (choice == 1) {
@@ -20,21 +21,34 @@ public class Main {
             System.out.println("Enter your email: ");
             String email = scanner.nextLine();
             Customer c = new Customer(name, userId, email);
-            c.showMenu();
-            System.out.println("Enter your choice: ");
-            int ch = scanner.nextInt();
-            if (ch == 1) {
-                library.displayBooks();
-            } else if (ch == 2) {
-                System.out.println("Enter ISBN of book you want to checkout: ");
-                int is = scanner.nextInt();
-                library.checkOutBook(is);
-            } else if (ch == 3) {
-                System.out.println("Enter ISBN of book you want to return: ");
-                int is = scanner.nextInt();
-                library.returnBook(is);
-            } else {
-                System.out.println("Enter valid choice!");
+            boolean running = true;
+            while (running) {
+                c.showMenu();
+                System.out.println("Enter your choice: ");
+                int ch = scanner.nextInt();
+                switch (ch) {
+                    case 1:
+                        library.displayBooks();
+                        break;
+                    case 2: {
+                        System.out.println("Enter ISBN of book you want to checkout: ");
+                        int is = scanner.nextInt();
+                        library.checkOutBook(is);
+                        break;
+                    }
+                    case 3: {
+                        System.out.println("Enter ISBN of book you want to return: ");
+                        int is = scanner.nextInt();
+                        library.returnBook(is);
+                        break;
+                    }
+                    case 4: {
+                        running = false;
+                        break;
+                    }
+                    default:
+                        System.out.println("Enter valid choice!");
+                }
             }
         } else if (choice == 2) {
             System.out.println("Enter your userId: ");
